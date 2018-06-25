33520645cb1eba6c866df19ba76061d805c7c7bf
hanjyjv <1261854356@qq.com>
2018-06-25 20:54:56 +0800

    the first three

--- Sequence.cpp
+++ Sequence.cpp
@@ -1,19 +1,20 @@
 #include<iostream>
-#include<string>
 #include<fstream>
+#include<string>
+#include<cstring>
 #include"Sequence.h"
 using namespace std;
 
 Sequence::Sequence(string filename)
 {
-    ifstream in;
-    in.open(filename);
-    if(in.is_open())
+    ifstream inf;
+    inf.open(filename.data());
+    while(!inf.eof())
     {
-      while(!in.eof())
-           in>>s;
+        getline(inf,s);
+        s2+=s;
     }
-    in.close();
+    inf.close();
 }
 
 Sequence::~Sequence()
@@ -22,11 +23,6 @@ Sequence::~Sequence()
 
 int Sequence::length()
 {
-    int num=s.length()%101;
-    for(int i=1; i<num; i++)
-    {
-       s2=s.erase(i*100,1);
-    }
     n=s2.length();
     cout<<"length:"<<endl;
     cout<<n<<endl;
@@ -34,53 +30,76 @@ int Sequence::length()
 
 int Sequence::numberOf(char base)
 {
-   int a=0;
-   if(base=='A')
-   {
-     for(int i=0;i<n;i++)
-     {
-        if(s[i]=='A')
-         a++;
-     }
-   }
+    int acount=0;
+    int tcount=0;
+    int ccount=0;
+    int gcount=0;
 
-   if(base=='T')
-   {
-     for(int i=0;i<n;i++)
-     {
-        if(s[i]=='T')
-          a++;
-     }
-   }
+    for(int i=0;i<n;i++)
+    {
+        switch(s2[i])
+        {
+            case 'A':
+                acount++;
+                break;
+            case 'T':
+                tcount++;
+                break;
+            case 'C':
+                ccount++;
+                break;
+            case 'G':
+                gcount++;
+                break;
+            default:
+                cout<<"False!"<<endl;
+                break;
+        }
+    }
+    
+    if(base=='A')
+        cout<<"A: "<<acount<<endl;
+    if(base=='T')
+        cout<<"T: "<<tcount<<endl;
+    if(base=='C')
+        cout<<"C: "<<ccount<<endl;
+    if(base=='G')
+        cout<<"G: "<<gcount<<endl;
 
-   if(base=='C')
-   {
-     for(int i=0;i<n;i++)
-     {
-        if(s[i]=='C')
-          a++;
-     }
-   }
-   
-   if(base=='G')
-   {
-     for(int i=0;i<n; i++)
-     {
-        if(s[i]=='G')
-          a++;
-     }
-   }
-
-    cout<<"the number of "<<base<<" is: "<<endl;
-    cout<<a<<endl;
 }
 
 string Sequence::longestConsecutive()
 {
+    int now=0;
+    int max=-1;
+    string s3;
 
+    for(int i=0;i<n;i++)
+    {
+        if(s2[i]==s2[i+1])
+        {
+            now++;
+        }
+        else
+        {
+            now++;
+            
+            if(now>max)
+            {
+                max=now;
+                s3=s2[i];
+            }
+            
+            now=0;
+        }
+    }
+    
+    for(int i=0;i<max-1;i++)
+        cout<<s3;
+    cout<<s3<<endl;
 }
 
 string Sequence::longestRepeated()
 {
-
+    
 }

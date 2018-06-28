6c7f89e2d72916c626f15d3d79bd39e16ae0b8dc
hanjyjv <1261854356@qq.com>
2018-06-28 14:41:17 +0800

    add clock

--- main.cpp
+++ main.cpp
@@ -1,14 +1,28 @@
 #include<iostream>
 #include<string>
 #include"Sequence.h"
+#include<time.h>
 using namespace std;
 
 int main()
 {
-  Sequence obj("dna.txt");
-  obj.length();
-  obj.numberOf('A');
-  obj.longestConsecutive();
+    clock_t start,finish;
+    double totaltime;
+    start=clock();
+
+    Sequence obj("dna.txt");
+    cout<<"length :"<<endl;
+    cout<<obj.length()<<endl;
+    cout<<"number :"<<endl;
+    cout<<obj.numberOf('A')<<endl;
+    cout<<"the longest consecutive :"<<endl;
+    cout<<obj.longestConsecutive()<<endl;
+    cout<<"the longest repeated :"<<endl;
+    cout<<obj.longestRepeated()<<endl;
+   
+    finish=clock();
+    totaltime=(double)(finish-start)/CLOCKS_PER_SEC;
+    cout<<totaltime<<endl;
   
-  return 0;
+    return 0;
 }
